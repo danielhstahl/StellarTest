5587ac1a44d8b0cb6f4942ac1dcc4dff2b196582
Daniel Stahl <danstahl1138@gmail.com>
2018-03-04 07:28:44 -0600

    improved generality of payment and added asset issuance

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const checkAccount=accountKey=>{
     })
 }
 
-const sendMoney=(destinationAccount, amount)=>{
+const sendMoney=(keyPair, destinationAccount, amount, asset)=>{
     return server.loadAccount(destinationAccount).catch(StellarSdk.NotFoundError, err=>{
         throw new Error('Account does not exist!  You will not be charged a transaction fee')
     }).then(()=>{
@@ -24,10 +24,10 @@ const sendMoney=(destinationAccount, amount)=>{
     }).then(account=>{
         let transaction=new StellarSdk.TransactionBuilder(account).addOperation(StellarSdk.Operation.payment({
             destination:destinationAccount,
-            asset:StellarSdk.Asset.native(),
+            asset//:StellarSdk.Asset.native(),
             amount
         })).addMemo(StellarSdk.Memo.text('TestNet')).build()
-        transaction.sign(sourceKeys)
+        transaction.sign(keyPair)
         return server.submitTransaction(transaction)
     }).then(result=>{
         console.log('Successfully sent:', result)
@@ -36,7 +36,30 @@ const sendMoney=(destinationAccount, amount)=>{
     })
 }
 
-checkAccount(primary.publicKey).then(()=>sendMoney(secondary.publicKey, '50')).then(()=>{
+/*
+checkAccount(primary.publicKey).then(()=>sendMoney(sourceKeys, secondary.publicKey, '50', StellarSdk.Asset.native())).then(()=>{
     checkAccount(primary.publicKey)
     checkAccount(secondary.publicKey)
-})
+})*/
+const issueAsset=(accountKey, assetName)=>{
+    return new StellarSdk.Asset(
+        assetName, accountKey)
+}
+
+const trustAsset=(keyPair, asset, limit)=>{
+    return server.loadAccount(keyPair.publicKey())
+    .catch(StellarSdk.NotFoundError, err=>{
+        throw new Error('Account does not exist!  You will not be charged a transaction fee')
+    })
+    .then(account=>{
+        const transaction=new StellarSdk.TransactionBuilder(account).addOperation(StellarSdk.Operation.changeTrust({
+            asset, limit
+        })).build()
+        transaction.sign(keyPair)
+        return server.submitTransaction(transaction)
+    })
+    .catch(err=>{
+        console.error(err)
+    })
+}
+
